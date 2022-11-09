ed10b4eb516d7b2cb938b7c0e3af79f05b2d7a46
James Anh <james.anh184@gmail.com>
2022-11-08 22:56:58 -0800

    Arrays

---
+++ Objects&Arrays/Arrays.js
@@ -0,0 +1,14 @@
+// Big O of Arrays
+// insertion => depends
+// removal -> depends
+// Searching -> O(N)
+// Access - O(1)
+
+let names = ["James", "Jenny", "Xoi"]
+
+names.pop()
+// Big O of Array Operations
+
+// push() pop() => O(1)
+// shift() unshift() =>  O(n)
+// sort => O(N * Log N)
