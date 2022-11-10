56e228bc446f2f9a1920e42cffcdcf7c465da0c6
James Anh <james.anh184@gmail.com>
2022-11-09 23:28:39 -0800

    Sumzero

--- Objects&Arrays/MultiplePointers.js
+++ Objects&Arrays/MultiplePointers.js
@@ -38,3 +38,22 @@ function sumZero2(arr) {
 }
 
 sumZero2([-4,-3,-2,-1,0,3,4,6])
+
+// countUniqueValues
+// Implement a function called countUniqueValues, which accepts a sorted array
+// and counts the unique values in the array
+// there can be negative numbers in the array but will always be sorted
+
+function countUniqueValues(arr) {
+    if(arr.length === 0) return 0
+   var i = 0
+   for (let j = 1; j < arr.length; j++) {
+        if(arr[i] !== arr[j]) {
+            i++;
+            arr[i] = arr[j]
+        }
+     }
+     return i + 1
+    }
+
+countUniqueValues([1,2,3,4,5,6]) // output 6
