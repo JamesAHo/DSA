7242620ae162c4022371ea8d4cc6501aa38250fd
James Anh <james.anh184@gmail.com>
2022-11-10 13:26:20 -0800

    implement search function

--- Objects&Arrays/maxSubarraySum.js
+++ Objects&Arrays/maxSubarraySum.js
@@ -34,7 +34,39 @@ function maxSubarraySum2(arr,num) {
     }
     return maxSum;
 }
+// Search function
+// Given a sorted array of integers, write a function called search
+// that accepts a value and returns the index where the value passed to the function is located
+// If the value is not found, return -1
+// Linear search
+function search(arr, val) {
+    // typical approache 
+    // loop over array and search for value
+    for(let i = 0 ; i < arr.length; i++) {
+        if(arr[i] === val) {
+            return i;
+        }
+    }
+    return -1
+}
+// REFACTOR - better approach
+function search2(arr, val) {
+    let min = 0;
+    let max = arr.length -1;
 
+    while(min <= max) {
+        let middle = Math.floor(min + max) / 2;
+        let currentElement = arr[middle];
+        if(arr[middle] < val) {
+            min = middle + 1;
+        } else if (arr[middle] > val) {
+            max = middle -1;
+        }
+    }
+    return -1;
+}
+
+search2([2,3,5,6,2,6,7,3], 9)
 
 
 
