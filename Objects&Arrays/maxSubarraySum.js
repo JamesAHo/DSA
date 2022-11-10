0edded229aab4319d9b464bbcb6c4c360b1c0d86
James Anh <james.anh184@gmail.com>
2022-11-10 08:53:33 -0800

    added refactored version

--- Objects&Arrays/maxSubarraySum.js
+++ Objects&Arrays/maxSubarraySum.js
@@ -20,7 +20,20 @@ function maxSubarraySum(arr, num) {
     return max;
 }
 // REFACTOR solution
-
+function maxSubarraySum2(arr,num) {
+    let maxSum = 0;
+    let tempSum = 0;
+    if(arr.length < num) return null;
+    for(let i = 0; i < num; i++) {
+        maxSum += arr[i]
+    }
+    tempSum = maxSum;
+    for(let i = num; i < arr.length; i++) {
+        tempSum = tempSum - arr[i - num] + arr[i];
+        maxSum = Math.max(maxSum, tempSum)
+    }
+    return maxSum;
+}
 
 
 
