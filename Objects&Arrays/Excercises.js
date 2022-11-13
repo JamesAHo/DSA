00bf7233a3af2ba014333191ede069c7b14244d9
James Anh <james.anh184@gmail.com>
2022-11-12 23:35:47 -0800

    maxSubbarry solution

--- Objects&Arrays/Excercises.js
+++ Objects&Arrays/Excercises.js
@@ -54,4 +54,21 @@ function averagePair() {
     }
     return false;
 }
+// Given an array of integers and a number, write a function called maxSubarraySum, which finds the maximum sum of a subarray with the length of the number passed to the function.
+
+function maxSubarraySum(arr,num) {
+    // edge case if arr.length < num return null
+   let maxSum = 0;
+   let tempSum = 0;
+   if(arr.length < num ) return null;
+   for(let i = 0; i < num; i++) {
+        maxSum += arr[i];
+   }
+   tempSum = maxSum;
+   for(let i = num; i < arr.length; i++) {
+        tempSum = tempSum - arr[i - num] + arr[i];
+   }
+   return maxSubarraySum;
+
+}
 
