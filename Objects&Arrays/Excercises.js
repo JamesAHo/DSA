c6c6749f30f67b4ec4f28996f2015422230d33f0
James Anh <james.anh184@gmail.com>
2022-11-14 12:56:20 -0800

    minSubArrayLen

--- Objects&Arrays/Excercises.js
+++ Objects&Arrays/Excercises.js
@@ -73,4 +73,44 @@ function maxSubarraySum(arr,num) {
    return maxSum
 
 }
+// Given an integer array nums, find the subarray which has the largest sum and return its sum.
+// Maximum subarray
+function maxSubarraySum(nums) {
+    let maxSum = -Infinity;
+    let tempSum = 0;
+    for(let i = 0; i < nums.length; i++) {
+        tempSum = Math.max(nums[i], tempSum + nums[i])
+        maxSum = Math.max(maxSum, tempSum)
+    }
+    return maxSum;
+}
+// Find minSubArray Solution
+function minSubArrayLen(nums, sum) {
+    let total = 0;
+    let start = 0;
+    let end = 0;
+    let minLen = Infinity;
+   
+    while (start < nums.length) {
+      // if current window doesn't add up to the given sum then 
+          // move the window to right
+      if(total < sum && end < nums.length){
+        total += nums[end];
+              end++;
+      }
+      // if current window adds up to at least the sum given then
+          // we can shrink the window 
+      else if(total >= sum){
+        minLen = Math.min(minLen, end-start);
+              total -= nums[start];
+              start++;
+      } 
+      // current total less than required total but we reach the end, need this or else we'll be in an infinite loop 
+      else {
+        break;
+      }
+    }
+   
+    return minLen === Infinity ? 0 : minLen;
+  }
 
