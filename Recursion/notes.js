0780656d9b7880b45f60dd2818fb60db84ed38b5
James Anh <james.anh184@gmail.com>
2022-11-14 21:36:17 -0800

    simple base case

--- Recursion/notes.js
+++ Recursion/notes.js
@@ -2,3 +2,13 @@
 // understand the two essential components of a recursive function
 // Visualize the call stack to better debug and undersstand recursive functions
 // use helper method recursion and pure recursion to solve more difficult problems
+
+// recursion => a process that calls itself
+// Base Case => Different Input
+
+// SImple base case
+
+function sumRange(sum) {
+    if(num === 1) return 1
+    return num + sumRange(num -1)
+}
