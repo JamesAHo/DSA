8682474225972c5f2db0dd2d787b9e6f578b54d8
James Anh <james.anh184@gmail.com>
2022-11-15 21:33:19 -0800

    bug fixes

--- SearchAlgoithms/Search.js
+++ SearchAlgoithms/Search.js
@@ -4,8 +4,7 @@ function search(arr,value) {
    for( let i = 0; i < arr.length; i++) {
     if (arr[i] === value) {
         return i
-    } else {
-        return -1
-    }
-   } 
+    } 
+   }
+   return -1
 }
