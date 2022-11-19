01ba8beb50a2579a04c271b2b38609b637d0cb49
James Anh <james.anh184@gmail.com>
2022-11-18 22:16:12 -0800

    bubble sort notes

---
+++ BubbleSort/notes.js
@@ -0,0 +1,9 @@
+// sorting uis the process of rearranging the items in a collection. So that the items in some kind of order
+// sorting numbers from smallest to largest
+// sorting movies based on released year
+// sorting movies based on revenue
+// sortting is an increadibly common task, so it's good to know how it works
+// There are many diifferent ways top sort things, and different techniques have their own advantage
+
+
+
