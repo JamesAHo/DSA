a9eabee9065b4c00c41b2d545f881acdc74f9d4a
James Anh <james.anh184@gmail.com>
2022-11-18 22:51:16 -0800

    optomized solution

--- BubbleSort/notes.js
+++ BubbleSort/notes.js
@@ -27,3 +27,21 @@ function Bubblesort(arr) {
     }
     return arr;
 }
+// Bubblesort optomized solution
+function Bubblesort2(arr) {
+    var noSwap;
+    for(var  i =  arr.length; i > 0; i--) {
+        noSwap = true;
+        for(var j = 0; j < i -1;j++) {
+            console.log(arr, arr[j], arr[j+1]);
+            if(arr[j] > arr[j+1]) {
+                var temp = arr[j];
+                arr[j] = arr[j+1];
+                arr[j+1] = temp;
+                noSwap = false;
+            }
+        }
+        if(noSwap) break;
+    }
+    return arr;
+}
