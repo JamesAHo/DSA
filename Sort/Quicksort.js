0f2054601e3746a2035da20edf992857a9909f1c
James Anh <james.anh184@gmail.com>
2022-12-01 12:42:51 -0800

    quicksort

---
+++ Sort/Quicksort.js
@@ -0,0 +1,3 @@
+// Just like merge sort, exploits the fact that arrays of 0 or 1 element are always sorted
+// Works by selecting one element and finding the index where pivot should end up in the sorted
+// Once the pivot is positioned appropriately, quick sort can be applied on either side of the pivot. 
