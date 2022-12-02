e29cad710568ab6a5e5fea405f09a4fe0caeb8e2
James Anh <james.anh184@gmail.com>
2022-12-01 22:10:58 -0800

    pop method

--- linkedList/linkedList.js
+++ linkedList/linkedList.js
@@ -26,4 +26,22 @@ class singlyLinkedList {
         this.length++;
         return this
     }
+    // pop method -- removing a node from the end of the Linked List
+    pop() {
+        if(!this.head) return undefined;
+        let temp = this.head;
+        let newtail = temp;
+        while(temp.next) {
+            newtail = temp;
+            temp = temp.next;
+        }
+        this.tail = newtail;
+        this.tail.next = null;
+        this.length--;
+        if(this.length === 0) {
+            this.head = null;
+            this.tail = null;
+        }
+        return temp;
+    }
 }
