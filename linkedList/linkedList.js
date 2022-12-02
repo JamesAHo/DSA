be332d0006289ee445a79de66bbbe21f524a6b9c
James Anh <james.anh184@gmail.com>
2022-12-01 21:52:07 -0800

    added push method for singlyLinkedList

--- linkedList/linkedList.js
+++ linkedList/linkedList.js
@@ -1,1 +1,29 @@
 // Object Oriented Program
+// Singly Linkedlist
+// Each element is a node
+class Node {
+    constructor(value) {
+        this.value = value;
+        this.next = null;
+
+    }
+}
+class singlyLinkedList {
+    constructor() {
+        this.length = 0;
+        this.head = null;
+        this.tail = null;
+    }
+    push(value) {
+        const newNode = new Node(value);
+        if(!this.head) {
+            this.head = newNode;
+            this.tail = newNode;
+        } else {
+            this.tail.next = newNode;
+            this.tail = newNode
+        }
+        this.length++;
+        return this
+    }
+}
