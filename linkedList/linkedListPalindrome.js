fb141bc773c0323e738d3b8578c4882ec297cbfb
James Anh <james.anh184@gmail.com>
2022-12-06 20:32:13 -0800

    adjustment

--- linkedList/linkedListPalindrome.js
+++ linkedList/linkedListPalindrome.js
@@ -7,7 +7,7 @@ const findMidpoint = (head) => {
     let fast = head;
     while(fast && fast.next) {
         slow = slow.next;
-        fast = fast.next.next
+        fast = fast.next.next;
     }
     return slow;
 }
