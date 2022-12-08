4016e32b9b140a524afeb99152e77e7b22dc6170
James Anh <james.anh184@gmail.com>
2022-12-08 15:15:20 -0800

    lexical scope

---
+++ Javascript/lexicalScope.js
@@ -0,0 +1,19 @@
+// Lexical environment is the defination area of an expression. An item's lexical scope is the pace in which the item got created.
+// In javascript our lexical scope (available data + variables where the function was defined)
+// determines our avaible variables. Not where the function is called(dynamic scope)
+
+// lexical environment printName
+function printName() {
+    return 'James A Ho'
+};
+// lexical environment findName
+function findName() {
+    function a() {
+
+    }
+    return printName();
+}
+// lexical environment sayMyName
+function sayMyName() {
+    return findName();
+}
