0b5f3e004b7c2928699e6381b3a11cf073e9af20
James Anh <james.anh184@gmail.com>
2022-12-11 14:09:42 -0800

    call()

---
+++ Javascript/advanced.js
@@ -0,0 +1,20 @@
+// implementation of call() apply() bind();
+function a() {
+    console.log('hi')
+}
+a.call()
+// implementation game
+
+const wizard = {
+    name:'Banana',
+    health:50,
+    heal() {
+        return this.health = 100
+    }
+}
+const archer = {
+    name: 'Apple',
+    health: 30,
+    
+}
+wizard.heal.call(archer)
