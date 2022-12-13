fd8a4875291ce8ce9ec4be2b2648a1733e97572c
James Anh <james.anh184@gmail.com>
2022-12-13 14:27:11 -0800

    higher order function using arrow function

--- Javascript/higherOrder.js
+++ Javascript/higherOrder.js
@@ -4,6 +4,11 @@ const multiplyBy = function(num1) {
         return num1 * num2;
     }
 }
+// scalable codes
 const multiplyByTwo = multiplyBy(2);
+const multiplyByTen = multiplyBy(10);
 multiplyByTwo(4);
 multiplyByTwo(8);
+
+// a more efficient way  is to use arrow functions
+const multiplyByy = (num1) => (num2) => num1 * num2
