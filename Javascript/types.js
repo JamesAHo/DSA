5faad0e257ca48390fec5c369da7196092f1f0f1
James Anh <james.anh184@gmail.com>
2022-12-12 16:34:14 -0800

    javascript types

---
+++ Javascript/types.js
@@ -0,0 +1,17 @@
+// Primitive
+typeof 5
+typeof true
+typeof 'to be or not to be'
+typeof undefined
+typeof null
+typeof Symbol
+
+// Non-Primitive
+const obj1 = {
+    a: 'Tom'
+}
+typeof {}
+typeof []
+typeof function(){}
+// Type numbers
+typeof Infinity 
