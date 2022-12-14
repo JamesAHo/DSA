1ddc388e8fde440b766f24a06602498b38ebf4b5
James Anh <james.anh184@gmail.com>
2022-12-14 09:38:22 -0800

    4 ways of this keyword

--- Javascript/OOP.js
+++ Javascript/OOP.js
@@ -12,3 +12,44 @@ class Elf {
 }
 const james = new Elf('James','Gun');
 console.log(james.attack())
+
+// 4 ways to interact with this keyword
+
+// new binding this
+function Person(name,weapon) {
+    this.name = name;
+    this.weapon = weapon;
+}
+const Jack = new Person('Jack',35)
+
+// implicit binding this
+const person = {
+    name: 'Jack',
+    age: 35,
+    hi() {
+        console.log('hi' + this.name)
+    }
+}
+
+// explicit binding this
+// using call bind apply to explicitly tell the program
+const person3 = {
+    name:'Jenny',
+    age:30,
+    hi: function() {
+        console.log('hi' + this.setTimeout)
+    }.bind(window)
+}
+
+// arrow function
+// lexical scoping
+const person4 = {
+    name: 'Karen',
+    age:30,
+    hi: function() {
+    var inner = () => {
+        console.log('hi' + this.name)
+    }
+    return inner()
+}
+}
