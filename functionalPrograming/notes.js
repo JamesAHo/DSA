6b8bec36b4f51c2cac25b2a09aace2b133c69987
James Anh <james.anh184@gmail.com>
2022-12-15 16:53:47 -0800

    Indempotence

---
+++ functionalPrograming/notes.js
@@ -0,0 +1,24 @@
+// Example
+// Amazon shipping
+const user = {
+    name:'James',
+    active: true,
+    cart: [],
+    purchases: []
+}
+// Implement a cart features: 
+// 1. Add items to cart
+// 2. Add 3% tax to item in cart
+// 3. Buy item: cart --> purchases
+// 4. Empty cart
+
+// ** Pure Function === no side effect
+
+// Indempotence
+// Idempotence is any function that can be executed several times without changing the final result beyond its first iteration. Idempotence is a technical word, used in mathematics and computer science, that classifies a function's behavior
+function notGood(num) {
+    return Math.random(num)
+}
+notGood()
+// Imperative vs Declerative
+
