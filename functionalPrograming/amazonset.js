769b793ed934ca3bfbc832628a1321b93509faa5
James Anh <james.anh184@gmail.com>
2022-12-15 23:04:32 -0800

    functional programming compose

---
+++ functionalPrograming/amazonset.js
@@ -0,0 +1,48 @@
+// Example
+// Amazon shipping
+const user = {
+    name:'James',
+    active: true,
+    cart: [],
+    purchases: []
+}
+// using compose method
+const amazonHistory = []
+const compose = (f,g) => (...args) => f(g(...args));
+
+purchaseItem(
+    emptyCart,
+    buyItem,
+    applyTaxToItems,
+    itemToCart
+)(user,{name:'laptop',price:200})
+function purchaseItem(...fns) {
+    return fns.reduce(compose)
+}
+function itemToCart(user, item) {
+    const updatedCart = user.cart.concat([item]);
+    return Object.assign({},user,{cart:updatedCart})
+}
+function applyTaxToItems(user) {
+    const {cart} = user;
+    const taxRate = 1.3;
+    const updatedCart = cart.map(item => {
+        return {
+            name:item.name,
+            price: item.price * taxRate
+        }
+    })
+    return Object.assign({},user,{cart:updatedCart});
+}
+function buyItem(user) {
+    return Object.assign({}, user, {purchases:user.cart})
+}
+function emptyCart(user) {
+    return Object.assign({},user,{cart:[]});
+}
+// Implement a cart features: 
+// 1. Add items to cart
+// 2. Add 3% tax to item in cart
+// 3. Buy item: cart --> purchases
+// 4. Empty cart
+
---
+++ functionalPrograming/compose&pipe.js
@@ -0,0 +1,22 @@
+// Compose
+// In JavaScript, the compose and pipe functions are higher-order functions that allow you to combine multiple functions into a single function. These functions are commonly used in functional programming to create more complex or customized behavior by combining simple functions in various ways.
+// The compose function combines multiple functions such that the output of one function is passed as the input to the next function. It applies the functions from right to left, so the output of the last function in the list is passed as the input to the first function.
+
+// example
+
+const compose = (f,g) => (data) => f(g(data))
+const multiplyBy3 = (num) => num*3;
+const makePositive = (num) => Math.abs(num);
+const multiplyBy3ThenAbs = compose(multiplyBy3,makePositive,makePositive);
+
+multiplyBy3ThenAbs(-50);
+
+// PIPE
+// The pipe function is similar to compose, but it applies the functions from left to right, so the output of the first function in the list is passed as the input to the second function. Here is an example of using pipe to create a new function that applies two functions in sequence:
+const pipe = (...fns) => (x) => fns.reduce((v, f) => f(v), x);
+
+const add1 = (x) => x + 1;
+const mul2 = (x) => x * 2;
+
+const mul2ThenAdd1 = pipe(mul2, add1);
+console.log(mul2ThenAdd1(3));  // 7
