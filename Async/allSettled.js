0007c46527e8237bc29f5087800f9af7739022fd
James Anh <james.anh184@gmail.com>
2022-12-20 15:56:53 -0800

    promise all

--- Async/allSettled.js
+++ Async/allSettled.js
@@ -2,4 +2,7 @@
 const promise1 = new Promise((resolve,reject) => setTimeout(resolve, 3000));
 const promise2 = new Promise((resolve,reject) => setTimeout(reject, 3000));
 
+// promise all
+Promise.all([promise1, promise2]).then(data => console.log(data));
 Promise.allSettled([promise1, promise2]).then(data => console.log(data)).catch(e => console.log('error', e));
+
