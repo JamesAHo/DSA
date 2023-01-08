507343c84c0634f40284b9408566cdc76a6e0a86
James Anh <james.anh184@gmail.com>
2023-01-07 22:37:20 -0800

    unshift method

--- linkedList/DoublyLL.js
+++ linkedList/DoublyLL.js
@@ -60,5 +60,24 @@ class DoublyLinkedList{
         this.length--;
         return temp;
     }
+    // unshift method
+    unshift(value){
+        const newNode = new Node(val)
+        if(this.length === 0) {
+            this.head = newNode;
+            this.tail = newNode;
+        } else {
+            this.head.prev = newNode;
+            newNode.next= this.head;
+            this.head = newNode;
+        }
+        this.length++;
+        return this
+        
+        
+
+        // 1 2
+        // temp 1 2 
+    }
     
 }
