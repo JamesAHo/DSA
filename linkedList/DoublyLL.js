e68baaa4c6b9dca6a3e2d743c5b64a473b144695
James Anh <james.anh184@gmail.com>
2023-01-10 23:09:58 -0800

    reverse

--- linkedList/DoublyLL.js
+++ linkedList/DoublyLL.js
@@ -134,4 +134,17 @@ class DoublyLinkedList{
         this.length--;
         return removedNode;
     }
+    reverse(head){
+        let current = this.head;
+        let temp = null;
+        while(current !== null){
+            temp = current.prev;
+            current.prev = current.next;
+            current.next = temp;
+            current = current.prev
+        }
+        if(temp !== null){
+            head = temp.prev
+        }
+    }
 };
