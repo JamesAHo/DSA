f5bf776ce65e5f3a27e12e8d4643c84eb89f86e8
James Anh <james.anh184@gmail.com>
2023-02-02 20:19:19 -0800

    pop method

--- Stack/stack.js
+++ Stack/stack.js
@@ -23,4 +23,14 @@
         }
         return ++this.size;
     }
+    pop(){
+        if(!this.first) return null;
+        var temp = this.first;
+        if(this.first === this.last){
+            this.last = null;
+        }
+        this.first = this.first.next;
+        this.size--;
+        return temp.value;
+    }
  }
