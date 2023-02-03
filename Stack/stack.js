e8bfe7ba51179a44d1ceb4c295ba77f21f53a593
James Anh <james.anh184@gmail.com>
2023-02-02 20:16:02 -0800

    stack push method

--- Stack/stack.js
+++ Stack/stack.js
@@ -1,1 +1,26 @@
 // Last In First Out principle. It has two main operations : push and pop
+ class Node {
+    constructor(value) {
+        this.value = value;
+        this.next = null;
+    }
+ }
+ class Stack {
+    constructor(value) {
+        this.first = null;
+        this.last = null;
+        this.size = 0
+    }
+    push(value) {
+        var newNode = new Node(value);
+        if(!this.first){
+            this.first = newNode;
+            this.last = newNode;
+        } else {
+            var temp = this.first;
+            this.first = newNode;
+            this.first.next = temp;
+        }
+        return ++this.size;
+    }
+ }
