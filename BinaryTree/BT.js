e57fbc3611f9537b9ddfb777302e94737e3cdfdd
James Anh <james.anh184@gmail.com>
2023-02-06 21:51:04 -0800

    Binary Search Tree

--- BinaryTree/BT.js
+++ BinaryTree/BT.js
@@ -1,1 +1,14 @@
 // A binary tree is a tree data structure in which each node has at most two children, which are referred to as the left child and the right child.
+class Node {
+    constructor(value) {
+        this.value = value;
+        this.left = null;
+        this.right = null;
+    }
+}
+
+class BST {
+    constructor(){
+        this.root = null;
+    }
+}
