e501e9b065cf58f9213d8de52fbe66b104707f0d
James Anh <james.anh184@gmail.com>
2023-02-06 22:12:59 -0800

    implementation BST

--- BinaryTree/BT.js
+++ BinaryTree/BT.js
@@ -11,4 +11,31 @@ class BST {
     constructor(){
         this.root = null;
     }
+    // insert Method
+    insert(value) {
+        var newNode = new Node(value);
+        if(this.root === null) {
+            this.root = newNode;
+            return this;
+        } else {
+            var temp = this.root;
+            while(true){
+                if(value < temp.value) {
+                    if(temp.left === null){
+                        temp.left = newNode;
+                        return this;
+                    } else {
+                        temp = temp.left;
+                    }
+                } else if(value > temp.value){
+                    if(temp.right === null){
+                        temp.right = newNode;
+                        return this;
+                    } else {
+                        temp = temp.right;
+                    }
+                }
+            }
+        }
+    }
 }
