b07a4b70b2a9ebc29a3ca9593aa38cb9844f5c00
Jenny Tran <piggy@Jennys-MacBook-Pro.local>
2023-02-22 23:46:23 -0800

    fixes

---
+++ Leet150/topK.js
@@ -0,0 +1,6 @@
+// Given an integer array nums and an integer k, return the k most frequent elements. You may return the answer in any order.
+function topKFrequent(nums,k){
+    let counter1 = {};
+    let counter2 = {};
+    
+}
