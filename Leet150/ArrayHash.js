71a69b317be5b55ae3eb449ad59f63fb3f78c4cf
James Anh <james.anh184@gmail.com>
2023-03-02 07:26:14 -0800

    fixes

--- Leet150/ArrayHash.js
+++ Leet150/ArrayHash.js
@@ -32,7 +32,7 @@ var twoSum = function(nums, target) {
     for(var i = 0; i < nums.length; i++){
         for(var j = i + 1; j < nums.length; j++){
             if(nums[i] + nums[j] == target){
-                result.push(i,j)
+                result.push(i,j);
             }
         }
     }
