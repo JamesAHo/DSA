8b9e7288f9132cbcc80ae618f29511ddb8980cd1
James Anh <james.anh184@gmail.com>
2023-03-06 15:24:56 -0800

    added vertex to graph

--- Graph/graph.js
+++ Graph/graph.js
@@ -1,1 +1,11 @@
-// Graph
+// Types of graph
+// Adjacency matrix
+// Adjacency List
+class Graph{
+    constructor(graph){
+        this.adjacency = {};
+    }
+    addVertex(vertex){
+        if(this.adjacencyList[vertex] = []) this.adjacencyList[vertex] = [];
+    }
+}
