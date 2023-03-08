f1264129e23dd02a233db91e14eaf4879dba3705
James Anh <james.anh184@gmail.com>
2023-03-07 21:50:10 -0800

    dfs recursive

--- Graph/graph.js
+++ Graph/graph.js
@@ -2,7 +2,7 @@
 // Adjacency matrix
 // Adjacency List
 class Graph{
-    constructor(graph){
+    constructor(){
         this.adjacencyList = {};
     }
     addVertex(vertex){
@@ -25,5 +25,24 @@ class Graph{
             const adjacentVertex = this.adjacencyList[vertex].pop();
             this.removeEdge(vertex, adjacentVertex);
         }
+        delete this.adjacencyList[vertex];
+    }
+    // DFS recursive
+    DFS(start){
+        const result = [];
+        const visited = {};
+        const adjacencyList = this.adjacencyList;
+        (function dfs(vertex){
+            if(!vertex) return null;
+            visited[vertex] = true;
+            result.push(vertex);
+            adjacencyList[vertex].forEach(neighbor => {
+                if(!visited[neighbor]){
+                    return dfs(neighbor)
+                }
+            })
+        })(start)
     }
 }
+
+// DFS in Graph
