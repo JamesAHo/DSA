bdf41e7622892b78cf409a76be3745bcedbc0a6a
James Anh <james.anh184@gmail.com>
2023-03-22 21:04:40 -0700

    typo

--- DynamicProgramming/sub.js
+++ DynamicProgramming/sub.js
@@ -2,8 +2,8 @@
 function fib(n, memo = []){
     if(memo[n] !== undefined) return memo[n];
     if(n <= 2) return 1;
-    var ressult = fib(n-1, memo) + fib(n-2, memo);
-    memo[n] = ressult
-    return ressult;
+    var result = fib(n-1, memo) + fib(n-2, memo);
+    memo[n] = result
+    return result;
 
 }
