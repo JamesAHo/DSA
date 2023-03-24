c7980a5c4a4e5074f1e3fb4c643276e4f1cab69f
James Anh <james.anh184@gmail.com>
2023-03-24 07:23:46 -0700

    notes

--- DynamicProgramming/sub.js
+++ DynamicProgramming/sub.js
@@ -1,9 +1,15 @@
-// Dynamic programing with recursive solution
+// Dynamic programing with recursive solution + memorized\
+// Time complexity is O(n)
 function fib(n, memo = []){
     if(memo[n] !== undefined) return memo[n];
     if(n <= 2) return 1;
     var result = fib(n-1, memo) + fib(n-2, memo);
     memo[n] = result
     return result;
+}
 
+// non memorize solution
+function fib(n){
+    if(n <= 2) return 1;
+    return fib(n-1) + fib(n-2);
 }
--- linkedList/linkedList.js
+++ linkedList/linkedList.js
@@ -24,7 +24,7 @@ class singlyLinkedList {
         // otherwise tail.next = newNode 
         } else {
             this.tail.next = newNode;
-            this.tail = newNode
+            this.tail = newNode;
         }
         // increment the list length
         this.length++;
