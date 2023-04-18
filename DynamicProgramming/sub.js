af6adc0a57e2fa76c14768d01d881c001cbe0e84
James Anh <james.anh184@gmail.com>
2023-04-17 19:58:24 -0700

    notes

--- DynamicProgramming/sub.js
+++ DynamicProgramming/sub.js
@@ -5,7 +5,7 @@ function fib(n, memo = []){
     if(memo[n] !== undefined) return memo[n];
     if(n <= 2) return 1;
     var result = fib(n-1, memo) + fib(n-2, memo);
-    memo[n] = result
+    memo[n] = result;
     return result;
 }
 
